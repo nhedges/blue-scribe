7e38d8ff698366d5f79fc4c00a727368c33b72bd
Nicholas Hedges <nicholas.hedges98@gmail.com>
2024-01-12 12:34:50 -0700

    Add some function documentation

--- blue_scribe_server/apps/blue_scribe_plan/src/blue_scribe_plan_image.erl
+++ blue_scribe_server/apps/blue_scribe_plan/src/blue_scribe_plan_image.erl
@@ -12,27 +12,47 @@
 -define(NIF, "blue_scribe_plan_image").
 
 
+%% -----------------------------------------------------------------------------
+%% @doc Loads the NIF so the image processing functions will work.
+%% -----------------------------------------------------------------------------
 init() ->
     erlang:load_nif(code:lib_dir(?APP) ++ "/priv/" ++ ?NIF, 0).
 
+%% -----------------------------------------------------------------------------
+%% @doc Loads a PNG file from disk as an OpenCV Mat
+%% -----------------------------------------------------------------------------
 -spec do_load_png_file(Path :: string()) -> image_mat().
 do_load_png_file(_Path) ->
     erlang:nif_error(nif_library_not_loaded).
 
+%% -----------------------------------------------------------------------------
+%% @doc Counts how many OpenCV Mats are tracked in this module's NIF
+%% -----------------------------------------------------------------------------
 -spec do_count_mats() -> non_neg_integer().
 do_count_mats() ->
     erlang:nif_error(nif_library_not_loaded).
 
+%% -----------------------------------------------------------------------------
+%% @doc Crops the specified image top-left if the specified bounds are smaller
+%% than the original image; otherwise the original is returned.
+%% -----------------------------------------------------------------------------
 -spec do_crop_image(Mat :: image_mat(),
                     Height :: non_neg_integer(),
                     Width :: non_neg_integer()) -> image_mat().
 do_crop_image(_,_,_) ->
     erlang:nif_error(nif_library_not_loaded).
 
+%% -----------------------------------------------------------------------------
+%% @doc Encode an OpenCV Mat as PNG format.
+%% -----------------------------------------------------------------------------
 -spec do_png_encode(Mat :: image_mat()) -> binary().
 do_png_encode(_) ->
     erlang:nif_error(nif_library_not_loaded).
 
+%% -----------------------------------------------------------------------------
+%% @doc Does a scan of the OpenCV Mat and turs it into laser operations.
+%% No optimization is done at this stage.
+%% -----------------------------------------------------------------------------
 -spec do_image_to_plan(Mat :: image_mat(), PowerScale :: float()) -> laser_plan().
 do_image_to_plan(_,_) ->
     erlang:nif_error(nif_library_not_loaded).
