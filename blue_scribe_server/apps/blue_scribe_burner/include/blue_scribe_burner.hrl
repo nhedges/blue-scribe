347f82a8cbfe2c81de8934fd70e6cf3ce0c9dd89
Nicholas Hedges <nicholas.hedges98@gmail.com>
2024-01-13 13:17:58 -0700

    Add some more types

--- blue_scribe_server/apps/blue_scribe_burner/include/blue_scribe_burner.hrl
+++ blue_scribe_server/apps/blue_scribe_burner/include/blue_scribe_burner.hrl
@@ -13,6 +13,9 @@
          start_y :: integer() | undefined,
          commands :: [#laser_command{}]}).
 
+-type laser_cmdop() :: #laser_operation{} | #laser_command{}.
 -type laser_plan() :: [#laser_operation{} | #laser_command{}].
+
+-type serial_baud() :: 300|1200|4800|9600|19200|28800|38400|57600|115200.
 
 -endif.%?BLUE_SCRIBE_BURNER_HRL
