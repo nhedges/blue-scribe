0b03c1736a91295189a6d8c71a741b343e7c0681
Nicholas Hedges <nicholas.hedges98@gmail.com>
2024-01-17 21:32:40 -0700

    Add a stop function for blue scribe laser server

--- blue_scribe_server/apps/blue_scribe_burner/src/blue_scribe_laser.erl
+++ blue_scribe_server/apps/blue_scribe_burner/src/blue_scribe_laser.erl
@@ -2,7 +2,7 @@
 -behaviour(gen_server).
 
 -export([start_link/0, start_burn/1, home/0, corner_align_next/0, pause_burn/0,
-         resume_burn/0]).
+         resume_burn/0, stop/0]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2]).
 
@@ -39,6 +39,10 @@ pause_burn() ->
 -spec resume_burn() -> ok | {error, _}.
 resume_burn() ->
     gen_server:call(?MODULE, resume).
+
+-spec stop() -> ok.
+stop() ->
+    gen_server:stop(?MODULE).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
