070f160c79fbd8fb21fcf3669a2c6ae4c27ea442
Nicholas Hedges <nicholas.hedges98@gmail.com>
2024-02-24 23:12:48 -0700

    Add timeout of 5 minutes to blue_scribe_plan processes
    - Avoid process leaks

--- blue_scribe_server/apps/blue_scribe_plan/src/blue_scribe_plan.erl
+++ blue_scribe_server/apps/blue_scribe_plan/src/blue_scribe_plan.erl
@@ -3,12 +3,13 @@
 
 -export([load_plan/1, unload_plan/1, get_plan/1, get_dimensions/1, start_link/1]).
 
--export([init/1, handle_call/3, handle_cast/2]).%, handle_info/2]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2]).
 
 -include_lib("blue_scribe_burner/include/blue_scribe_burner.hrl").
 
 -define(CROP_X, 380).
 -define(CROP_Y, 380).
+-define(TIMEOUT, 300000). % 5 minutes
 
 -record(state,
         {plan, image}).
@@ -54,25 +55,31 @@ init([Id]) ->
             logger:error("~p: Error: plan ~p not found", [?MODULE, Id]),
             blue_scribe_plan_image:do_image_to_plan(ImgCropped)
     end,
-    {ok, #state{plan=Plan, image=Img}}.
+    {ok, #state{plan=Plan, image=Img}, ?TIMEOUT}.
 
 handle_call(get_plan, _From, #state{plan=Plan}=State) ->
-    {reply, {ok, Plan}, State};
+    {reply, {ok, Plan}, State, ?TIMEOUT};
 
 handle_call(get_dimensions, _From, #state{image=Img}=State) ->
     {W,H} = blue_scribe_plan_image:do_get_dimensions(Img),
-    {reply, {ok, {W,H}}, State};
+    {reply, {ok, {W,H}}, State, ?TIMEOUT};
 
 handle_call(Call, _, State) ->
     logger:warning("~p: Unknown call ~p", [?MODULE, Call]),
-    {reply, ok, State}.
+    {reply, ok, State, ?TIMEOUT}.
 
 handle_cast(unload, State) ->
     {stop, normal, State};
 
 handle_cast(Cast, State) ->
-    logger:warning("~p: Unknown call ~p", [?MODULE, Cast]),
-    {noreply, State}.
+    logger:warning("~p: Unknown cast ~p", [?MODULE, Cast]),
+    {noreply, State, ?TIMEOUT}.
 
+handle_info(timeout, State) ->
+    {stop, normal, State};
+
+handle_info(Info, State) ->
+    logger:warning("~p: Uknownn info ~p", [?MODULE, Info]),
+    {no_reply, State}.
 
 
